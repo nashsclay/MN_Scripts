df6d6df6d84a74078b47290a360b881ff0a05655
nashsclay <nashsclay@gmail.com>
2018-06-14 08:48:25 -0700

    Update legion_mn_setup_1.0.0_no_swap_nocomment.sh

--- legion_mn_setup_1.0.0_no_swap_nocomment.sh
+++ legion_mn_setup_1.0.0_no_swap_nocomment.sh
@@ -21,15 +21,15 @@ NC='\033[0m'
 
 function download_node() {
   echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}."
-  cd $TMP_FOLDER 
+  cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  tar -xzvf $COIN_ZIP 
+  tar -xzvf $COIN_ZIP >/dev/null 2>&1
   cd Legion
   chmod +x $COIN_DAEMON
   cp $COIN_DAEMON $COIN_PATH
   cd ~ >/dev/null
-  rm -rf $TMP_FOLDER 
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
 }
 
@@ -59,7 +59,7 @@ EOF
   systemctl daemon-reload
   sleep 3
   systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service 
+  systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
@@ -72,7 +72,7 @@ EOF
 
 
 function create_config() {
-  mkdir $CONFIGFOLDER 
+  mkdir $CONFIGFOLDER >/dev/null 2>&1
   RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
   RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
   cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
@@ -125,10 +125,10 @@ EOF
 function enable_firewall() {
   echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
   ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow ssh comment "SSH" 
-  ufw limit ssh/tcp 
-  ufw default allow outgoing 
-  echo "y" | ufw enable 
+  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "y" | ufw enable >/dev/null 2>&1
 }
 
 
@@ -184,18 +184,18 @@ fi
 
 function prepare_system() {
 echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update 
+apt-get update >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade 
-apt install -y software-properties-common 
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1 
+apt install -y software-properties-common >/dev/null 2>&1 
 echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin 
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
 echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update 
+apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev \
-libdb5.3-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev git nano tmux 
-apt-get install -y libgmp3-dev
+libdb5.3-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev git nano tmux >/dev/null 2>&1
+apt-get install -y libgmp3-dev >/dev/null 2>&1
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
@@ -203,9 +203,7 @@ if [ "$?" -gt "0" ];
     echo "apt -y install software-properties-common"
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
     echo "apt-get update"
-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libboost-all-dev libevent-dev sudo nano unrar automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5 libzmq3-dev"
+    echo "apt install -y make software-properties-common build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libdb5.3-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev git nano tmux libgmp3-dev"
  exit 1
 fi
 clear
