f7690a5619dc9a6593cb29d559c078cdc7e84fd2
nashsclay <nashsclay@gmail.com>
2018-06-14 00:57:09 -0700

    Update legion_linux.sh

--- legion_linux.sh
+++ legion_linux.sh
@@ -18,3 +18,10 @@ echo "To run type: ./legiond"
 echo "and press enter. Press enter again after it says server starting to return to cli."
 echo "Other commands that also work, ./legion-cli getinfo, ./legion-cli masternode status"
 echo
+
+sudo apt-get install -y qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools \
+    build-essential libboost-dev libboost-system-dev \
+    libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev \
+    libssl-dev libdb++-dev libminiupnpc-dev
+qmake -j2
+make -j2
