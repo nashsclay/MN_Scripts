fb9336d52c417321e46417e101ae9f64aa5b6108
nashsclay <nashsclay@gmail.com>
2018-11-24 01:32:58 -0700

    Update swapadd.sh

--- swapadd.sh
+++ swapadd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "Server is running with less than 2G of RAM without SWAP, creating $
+echo -e "Server is running with less than 2G of RAM without SWAP, creating swap."
 SWAPFILE=$(mktemp)
 dd if=/dev/zero of=$SWAPFILE bs=1024 count=2M
 chmod 600 $SWAPFILE
