69513996f767384072f7b3e3c4c4e4624e72521d
nashsclay <nashsclay@gmail.com>
2018-11-24 01:26:04 -0700

    Create swapadd.sh

---
+++ swapadd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+    echo -e "Server is running with less than 2G of RAM without SWAP, creating $
+    SWAPFILE=$(mktemp)
+    dd if=/dev/zero of=$SWAPFILE bs=1024 count=2M
+    chmod 600 $SWAPFILE
+    mkswap $SWAPFILE
+    swapon -a $SWAPFILE
