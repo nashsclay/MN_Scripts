fc6667fc0f6e3ceada16bad5d4e8c7f11a305189
nashsclay <nashsclay@gmail.com>
2019-05-02 17:20:35 -0700

    Update eca_bootstrap_linux.sh

--- eca_bootstrap_linux.sh
+++ eca_bootstrap_linux.sh
@@ -1,1 +1,4 @@
-
+cd ~/Downloads
+wget https://github.com/Electra-project/Electra-Bootstrap/releases/download/v1.0/ElectraBootstrap.zip
+unzip ElectraBootstrap.zip
+Move files to .electra folder or .Electra
