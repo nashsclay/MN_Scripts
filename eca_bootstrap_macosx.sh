df6840f381dc8bd26c44f08445fc9c3ff9406041
nashsclay <nashsclay@gmail.com>
2019-05-03 21:07:19 -0700

    Update eca_bootstrap_macosx.sh

--- eca_bootstrap_macosx.sh
+++ eca_bootstrap_macosx.sh
@@ -4,19 +4,18 @@ echo "This script was forked from CryptoNeverSleeps"
 echo
 
 cd /usr/local/bin/
-sudo apt-get install unzip
 echo "Script may appear frozen for a few seconds. This is normal."
-wget -c https://github.com/Electra-project/Electra-Bootstrap/releases/download/v1.0/ElectraBootstrap.zip
+curl -k -O -L https://github.com/Electra-project/Electra-Bootstrap/releases/dow$
 sudo mkdir fix_electra_bootstrap
 sudo unzip -o ElectraBootstrap.zip -d fix_electra_bootstrap
 cd fix_electra_bootstrap
-sudo rm -R ~/.electra/blocks/
+sudo rm -R ~/Library/Application Support/.electra/blocks/
 sudo rm -R ~/.electra/chainstate/
 sudo rm -R ~/.electra/database/
 sudo rm -R ~/.electra/blocks/
 sudo rm -R ~/.electra/sporks/
 sudo rm -R ~/.electra/zerocoin/
-sudo rm ~/.electra/budget.dat ~/.electra/db.log ~/.electra/debug.log ~/.electra/fee_estimates.dat ~/.electra/mncache.dat ~/.electra/mnpayments.dat ~/.electra/peers.dat ~/.electra/.lock
+sudo rm ~/.electra/budget.dat ~/.electra/db.log ~/.electra/debug.log ~/.electra$
 sudo mv chainstate blocks sporks zerocoin peers.dat ~/.electra
 cd /usr/local/bin/
 sudo rm -R fix_electra_bootstrap
