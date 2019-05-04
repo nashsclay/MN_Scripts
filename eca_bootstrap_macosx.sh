3ef23ff6a1c63c7383d71da28014ea597cbc14a5
nashsclay <nashsclay@gmail.com>
2019-05-03 21:25:03 -0700

    Update eca_bootstrap_macosx.sh

--- eca_bootstrap_macosx.sh
+++ eca_bootstrap_macosx.sh
@@ -5,18 +5,18 @@ echo
 
 cd /usr/local/bin/
 echo "Script may appear frozen for a few seconds. This is normal."
-curl -k -O -L https://github.com/Electra-project/Electra-Bootstrap/releases/dow$
+curl -k -O -L https://github.com/Electra-project/Electra-Bootstrap/releases/download/v1.0/ElectraBootstrap.zip
 sudo mkdir fix_electra_bootstrap
 sudo unzip -o ElectraBootstrap.zip -d fix_electra_bootstrap
 cd fix_electra_bootstrap
-sudo rm -R ~/Library/Application Support/.electra/blocks/
-sudo rm -R ~/.electra/chainstate/
-sudo rm -R ~/.electra/database/
-sudo rm -R ~/.electra/blocks/
-sudo rm -R ~/.electra/sporks/
-sudo rm -R ~/.electra/zerocoin/
-sudo rm ~/.electra/budget.dat ~/.electra/db.log ~/.electra/debug.log ~/.electra$
-sudo mv chainstate blocks sporks zerocoin peers.dat ~/.electra
+sudo rm -R "~/Library/Application Support/Electra/blocks/"
+sudo rm -R "~/Library/Application Support/Electra/chainstate/"
+sudo rm -R "~/Library/Application Support/Electra/database/"
+sudo rm -R "~/Library/Application Support/Electra/blocks/"
+sudo rm -R "~/Library/Application Support/Electra/sporks/"
+sudo rm -R "~/Library/Application Support/Electra/zerocoin/"
+sudo rm "~/Library/Application Support/Electra/budget.dat" "~/Library/Application Support/Electra/db.log" "~/Library/Application Support/Electra/debug.log" "~/Library/Application Support/Electra/fee_estimates.dat" "~/Library/Application Support/Electra/mncache.dat" "~/Library/Application Support/Electra/mnpayments.dat" "~/Library/Application Support/Electra/peers.dat" "~/Library/Application Support/Electra/.lock"
+sudo mv chainstate blocks sporks zerocoin peers.dat "~/Library/Application Support/Electra"
 cd /usr/local/bin/
 sudo rm -R fix_electra_bootstrap
 sudo rm ElectraBootstrap.zip
